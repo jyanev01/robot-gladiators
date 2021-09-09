fd1dce24ba6e2611020dff1f7d378295927d5719
jyanev01 <jordanyanev@gmail.com>
2021-09-08 23:41:35 -0400

    startGame()

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,31 +65,36 @@ var fight = function (enemyName) {
     }
 };        
 
-// fight each enemy-robot by looping over them and fighting them one at a time
-for(var i = 0; i < enemyNames.length; i++){
-    //if player is still alive, keep fighting
-    if  (playerHealth > 0){
-        //let players know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-        window.alert("Welcome to Robot Gladiators! Round " + ( i+1));
+// function to start a new game
+var startGame = function() {
 
-        //pick new enemy to fight based on the index of the enemyNames array
-        var pickedEnemyName = enemyNames [i];
+    // fight each enemy-robot by looping over them and fighting them one at a time
+    for(var i = 0; i < enemyNames.length; i++){
+        //if player is still alive, keep fighting
+        if  (playerHealth > 0){
+            //let players know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+            window.alert("Welcome to Robot Gladiators! Round " + ( i+1));
 
-        // reset enemyHealth before starting new fight
-        enemyHealth = 50;
+            //pick new enemy to fight based on the index of the enemyNames array
+            var pickedEnemyName = enemyNames [i];
 
-        //use debugger to pause script from running and check waht's going on at that moment in the code
-        //debugger;
+            // reset enemyHealth before starting new fight
+            enemyHealth = 50;
 
-        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-        fight(pickedEnemyName);
+            //use debugger to pause script from running and check waht's going on at that moment in the code
+            //debugger;
+
+            // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            fight(pickedEnemyName);
+        }
+        // if player isn't alive, stop the game
+        else {
+            window.alert("You have lost your robot in the battle! Game Over!");
+            break;
+        }
+
     }
-    // if player isn't alive, stop the game
-    else {
-        window.alert("You have lost your robot in the battle! Game Over!");
-        break;
-    }
+};
 
-}
-
-
+// start the game when the page load
+startGame();
