a80847e8751ad65d440df674d4d3b6e36fe2c1ff
jyanev01 <jordanyanev@gmail.com>
2021-09-09 08:55:28 -0400

    create shop() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -91,6 +91,16 @@ var startGame = function() {
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+
+            // if we're not at the last enemy in the array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                // ask if the player ants to use the store before next round
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                // if yes, take them to the store() function
+                if(storeConfirm) {
+                    shop();
+            }
         }
         // if player isn't alive, stop the game
         else {
@@ -117,6 +127,13 @@ var endGame = function() {
     }
     window.alert("The game has now ended. Let's see how you did!");
 }
+
+var shop = function() {
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFIL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    
+};
+
 
 // asks the player if they'd like to play again
 var playAgainConfirm = window.confirm("Would you like to play again?");
